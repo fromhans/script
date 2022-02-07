e1fa3a544558640f4942ba7461af4345d25811d3
Hansu Kim <zihs0822@gmail.com>
2022-02-07 13:28:56 +0900

    "수정: ifconfig IP 저장 부분 수정"

--- install_kafka.sh
+++ install_kafka.sh
@@ -8,8 +8,7 @@ tar -zxvf kafka_2.13-3.1.0.tgz
 
 TARGET_DIRS="/opt/kafka/kafka_2.13-3.1.0"
 cd ${TARGET_DIRS}/config
-server_ip=`ifconfig eth0 | grep 'inet' | cut -d: -f2 | awk '{ print $2}'`
-	-> ifcfg eth0 | grep 'inet ' | awk
+server_ip=`ifconfig eth0 | grep 'inet ' | awk '{ print $2}'`
 zookeeper_ip=${server_ip}:2181
 sed -i -e 's,zookeeper.connect=localhost:2181,#zookeeper.connect=localhost:2181,g' ./server.properties
 echo -e "zookeeper.connect=${zookeeper_ip}" >> ./server.properties
@@ -17,4 +16,3 @@ echo -e "server.1=${server_ip}:2888:3888" >> ./zookeeper.properties
 
 nohup ${TARGET_DIRS}/bin/zookeeper-server-start.sh ${TARGET_DIRS}/config/zookeeper.properties 1>/dev/null 2>&1 &
 nohup ${TARGET_DIRS}/bin/kafka-server-start.sh ${TARGET_DIRS}/config/server.properties 1>/dev/null 2>&1 &
-
