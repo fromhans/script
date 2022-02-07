698df075d763252671a5acf929063aaf65334106
Hansu Kim <zihs0822@gmail.com>
2022-02-07 14:06:39 +0900

    Update get_kafka_script.sh

--- get_kafka_script.sh
+++ get_kafka_script.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
+#유저데이터에서 밀어넣는부분
 mkdir /usr/bin/custom_script
 cd /usr/bin/custom_script > /usr/bin/custom_script/exec_result.txt 2>&1
 wget https://raw.githubusercontent.com/fromhans/script/master/install_kafka.sh 1>>/usr/bin/custom_script/exec_result.txt 2>&1
 chmod 755 ./*
 sudo /usr/bin/sh ./install_kafka.sh
+/bin/sh ./install_kafka.sh
