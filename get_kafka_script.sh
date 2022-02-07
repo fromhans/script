88ba170633bbe99d838090dc01f1e76fb52c089b
Hansu Kim <zihs0822@gmail.com>
2022-02-07 14:23:06 +0900

    Update get_kafka_script.sh

--- get_kafka_script.sh
+++ get_kafka_script.sh
@@ -4,5 +4,4 @@ mkdir /usr/bin/custom_script
 cd /usr/bin/custom_script > /usr/bin/custom_script/exec_result.txt 2>&1
 wget https://raw.githubusercontent.com/fromhans/script/master/install_kafka.sh 1>>/usr/bin/custom_script/exec_result.txt 2>&1
 chmod 755 ./*
-sudo /usr/bin/sh ./install_kafka.sh
 /bin/sh ./install_kafka.sh
