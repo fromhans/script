19be979013479656996a8368aea00f678e7e507e
Hansu Kim <zihs0822@gmail.com>
2022-02-17 21:13:47 +0900

    Update install_kafka_3.1.0.sh

--- install_kafka_3.1.0.sh
+++ install_kafka_3.1.0.sh
@@ -17,4 +17,4 @@ echo "sh ${TARGET_DIRS}/kafka_startup.sh" >> /etc/rc.d/rc.local
 nohup ${TARGET_DIRS}/bin/zookeeper-server-start.sh ${TARGET_DIRS}/config/zookeeper.properties 1>>${TARGET_DIRS}/zookeeper_execute_log.txt 2>&1 &
 nohup ${TARGET_DIRS}/bin/kafka-server-start.sh ${TARGET_DIRS}/config/server.properties 1>>${TARGET_DIRS}/kafka_execute_log.txt 2>&1 &
 
-${TARGET_DIRS}/bin/kafka-topics.sh --bootstrap-server localhost:9092 --replication-factor 1 --partitions 1 --topic my_topic --create
+${TARGET_DIRS}/bin/kafka-topics.sh --bootstrap-server localhost:9092 --replication-factor 1 --partitions 1 --topic quickstart-events --create
