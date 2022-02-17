ef2e2ef964c7c67cc8a0377f7b2dcaf79e53d64c
Hansu Kim <zihs0822@gmail.com>
2022-02-17 18:33:27 +0900

    Update test_script.sh

--- test_script.sh
+++ test_script.sh
@@ -9,8 +9,8 @@ tar -zxvf kafka_2.13-3.1.0.tgz
 TARGET_DIRS="/opt/kafka/kafka_2.13-3.1.0"
 cd ${TARGET_DIRS}/config
 
-nohup ${TARGET_DIRS}/bin/zookeeper-server-start.sh ${TARGET_DIRS}/config/zookeeper.properties 1>/dev/null 2>&1 &
-nohup ${TARGET_DIRS}/bin/kafka-server-start.sh ${TARGET_DIRS}/config/server.properties 1>/dev/null 2>&1 &
+nohup ${TARGET_DIRS}/bin/zookeeper-server-start.sh ${TARGET_DIRS}/config/zookeeper.properties 1>>/bin/custom_script/kafka_execute_log.txt 2>&1 &
+nohup ${TARGET_DIRS}/bin/kafka-server-start.sh ${TARGET_DIRS}/config/server.properties 1>>/bin/custom_script/kafka_execute_log.txt 2>&1 &
 
-echo "nohup ${TARGET_DIRS}/bin/zookeeper-server-start.sh ${TARGET_DIRS}/config/zookeeper.properties 1>/dev/null 2>&1 &" >> /etc/rc.d/rc.local
-echo "nohup ${TARGET_DIRS}/bin/kafka-server-start.sh ${TARGET_DIRS}/config/server.properties" >> /etc/rc.d/rc.local
+echo "nohup ${TARGET_DIRS}/bin/zookeeper-server-start.sh ${TARGET_DIRS}/config/zookeeper.properties 1>>/bin/custom_script/kafka_execute_log.txt 2>&1" >> /etc/rc.d/rc.local
+echo "nohup ${TARGET_DIRS}/bin/kafka-server-start.sh ${TARGET_DIRS}/config/server.properties 1>>/bin/custom_script/kafka_execute_log.txt 2>&1" >> /etc/rc.d/rc.local
