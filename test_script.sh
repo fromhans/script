ec0727fde669222c5dccd77a7184dba7ee03f8ea
Hansu Kim <zihs0822@gmail.com>
2022-02-17 18:30:25 +0900

    Update test_script.sh

--- test_script.sh
+++ test_script.sh
@@ -8,11 +8,9 @@ tar -zxvf kafka_2.13-3.1.0.tgz
 
 TARGET_DIRS="/opt/kafka/kafka_2.13-3.1.0"
 cd ${TARGET_DIRS}/config
-server_ip=`ifconfig eth0 | grep 'inet ' | awk '{ print $2}'`
-zookeeper_ip=${server_ip}:2181
-sed -i -e 's,zookeeper.connect=localhost:2181,#zookeeper.connect=localhost:2181,g' ./server.properties
-echo "zookeeper.connect=${zookeeper_ip}" >> ./server.properties
-echo "server.1=${server_ip}:2888:3888" >> ./zookeeper.properties
 
 nohup ${TARGET_DIRS}/bin/zookeeper-server-start.sh ${TARGET_DIRS}/config/zookeeper.properties 1>/dev/null 2>&1 &
 nohup ${TARGET_DIRS}/bin/kafka-server-start.sh ${TARGET_DIRS}/config/server.properties 1>/dev/null 2>&1 &
+
+echo "nohup ${TARGET_DIRS}/bin/zookeeper-server-start.sh ${TARGET_DIRS}/config/zookeeper.properties 1>/dev/null 2>&1 &" >> /etc/rc.d/rc.local
+echo "nohup ${TARGET_DIRS}/bin/kafka-server-start.sh ${TARGET_DIRS}/config/server.properties" >> /etc/rc.d/rc.local
